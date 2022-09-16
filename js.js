72abb6142ba10bb982e3b23c7dd1da6811360c62
ashash <90792417+ashleychchan@users.noreply.github.com>
2022-09-16 12:29:02 -0400

    l

--- js.js
+++ js.js
@@ -1,16 +1,22 @@
 const oneButton= document.getElementById("dice");
 const twoButton= document.getElementById("die");
 const rollResult= document.getElementById('rollResult');
-const second= document.getElementById("second");
+
 const die= {
     numbers: 6, 
     roll: function () {
         let random = Math.floor(Math.random() * this.numbers) + 1;
         return random;
 }}
+function roll() {
+    console.log("this works");
+    let random = Math.floor(Math.random() * this.numbers) + 1;
+    return random;
+}
+
 function printResult(number){
-    
     rollResult.innerHTML= number;
+    second.innerHTML= number;
 }
 
 const button = document.getElementById('button');
@@ -22,12 +28,15 @@ button.onclick = function() {
 function twoDice(){
     const newElement = document.createElement("h2");
     newElement.id= "second"
-    newElement.innerHTML = "CONTENTS";
+    newElement.innerHTML = "dice";
     rollResult.appendChild(newElement); 
+    const second= document.getElementById("second");
+    second.addEventListener("click", roll);
     oneButton.disabled= true;
     twoButton.disabled = false;
 }
 function oneDie(){
+    const second= document.getElementById("second");
     second.remove("second"); 
     oneButton.disabled= false;
     twoButton.disabled= true;
